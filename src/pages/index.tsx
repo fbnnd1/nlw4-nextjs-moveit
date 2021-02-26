34bfb32a27ba76fe04d88785e34e1194f94f0951
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2021-02-25 23:48:17 -0300

    Aula 03: Context e inicio dos desafios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import {ExperienceBar} from '../components/ExperienceBar';
 import {Profile} from '../components/Profile';
 import { CompleteChallanges } from '../components/CompleteChallanges';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 
 export default function Home() {
@@ -20,7 +21,9 @@ export default function Home() {
           <CompleteChallanges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
