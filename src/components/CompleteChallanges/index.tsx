6de24201b5581841bc909bb276629e4b3b9aa3fa
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2021-02-28 21:30:17 -0300

    fix: Padronizacao de nomes dos arquivos de estilo

--- src/components/CompleteChallanges/index.tsx
+++ src/components/CompleteChallanges/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../../contexts/ChallengesContext';
-import styles from '../../styles/components/completeChallanges.module.css';
+import styles from '../../styles/components/CompleteChallanges.module.css';
 
 export function CompleteChallanges() {
     const { challengesCompleted } = useContext(ChallengesContext);
