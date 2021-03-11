a84fcc7112c7e08758bee7a240fecf7857ceae66
Fabio D7 Fatec <fabio.dias7@fatec.sp.gov.br>
2021-03-10 23:46:25 -0300

    Renomeacao de arquivos

--- src/components/CompleteChallenges.tsx
+++ src/components/CompleteChallenges.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
-import { ChallengesContext } from '../../contexts/ChallengesContext';
-import styles from '../../styles/components/CompleteChallenges.module.css';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/CompleteChallenges.module.css';
 
 export function CompleteChallenges() {
     const { challengesCompleted } = useContext(ChallengesContext);
